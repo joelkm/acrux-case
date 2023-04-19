b67d8183ac4bd8ee9f3e0ecb1bbc1a0c8224b1b9
JoelKM <joelkasmor16@gmail.com>
2023-04-19 16:09:21 +0200

    Setup basic server

---
+++ src/config/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const handleError = require('../middlewares/error-handler');
+const { NotFoundError } = require('./app-error');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cors(corsOptions));
+
+app.use("/", require("../views"));
+
+app.use("/user", require("../user"));
+
+app.use("*", (req, res, next) => {
+  next(new NotFoundError(`Could not handle ${req.method} request in '${req.protocol + '://' + req.get('host') + req.originalUrl}'`));
+});
+app.use((err, req, res, next) => {
+  handleError(err, req, res);
+});
+
+module.exports = app;
